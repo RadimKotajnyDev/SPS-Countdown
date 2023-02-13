9ad82849b0038a1317d887f18b4773d86607bdcc
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2023-02-13 18:26:49 +0100

    removed bloat code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import Timer from "./Timer.jsx";
 
 function App() {
     return (
-    <div className="bg-gradient-to-t bg-no-repeat from-[#0F2027] to-[#2C5364] bg-[#0F2027] text-white h-screen w-full flex items-center justify-center flex-col">
-        <h1 className="text-6xl font-light">Něco se chystá... 🤔</h1>
-        <div>
+        <div
+            className="bg-gradient-to-t bg-no-repeat from-[#0F2027] to-[#2C5364] bg-[#0F2027] text-white h-screen w-full flex items-center justify-center flex-col">
+            <h1 className="text-6xl font-light">Něco se chystá... 🤔</h1>
             <h2 className="font-semibold text-9xl">
-                <Timer />
+                <Timer/>
             </h2>
         </div>
-    </div>
-  )
+    );
 }
 
 export default App
